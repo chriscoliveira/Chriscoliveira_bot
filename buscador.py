1e1eb79fa815f1694fe2478fd104eb9786df10ac
Christian Carvalho <christian.coliveira@gmail.com>
2021-03-01 11:07:37 -0300

    Add files via upload

---
+++ buscador.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+
+
+def search(link):
+    pagina = requests.get(link)
+    soup = BeautifulSoup(pagina.text, 'html.parser')
+    # print(soup)
+    return soup
+
+
+def tiny_url(url):
+    api_url = 'https://tinyurl.com/api-create.php?url='
+    tinyurl = urllib.request.urlopen(api_url + url).read()
+    return tinyurl.decode('utf-8')
+
+
+def retorna_materias(link, tag_bloco, tag_titulo, tag_link=None, tag_horario=None, research=None):
+    pagina = requests.get(link)
+    soup = BeautifulSoup(pagina.text, 'html.parser')
+    arquivo = open('resultado.txt', 'w')
+    site = link.replace("www.", "").replace(".br", "").replace("/", "").replace('//','')
+    site = re.search(r'https:(.*?).com', site).group(1).upper()
+    arquivo.write(
+        f'\nNotícias de {site}\n')
+    contador = 0
+    for materia in soup.select(tag_bloco):
+        try:
+            texto = materia.select_one(tag_titulo).text
+            link = materia.select_one('a').get('href')
+
+            # verifica se existe a tag de horario da materia
+            if tag_horario:
+                horario = materia.select_one(tag_horario)
+                horario = horario.text.replace("há", "")
+            else:
+                horario = ''
+        except Exception as e:
+            texto = ''
+
+        if research:
+            if research.upper() in texto.upper():
+                # arquivo.write(f'{horario} - {texto.strip()} \n\n')
+                arquivo.write(f'{horario} - {texto.strip()} {tiny_url(link)}\n\n')
+                contador+=1
+
+        else:
+            # arquivo.write(f'{horario} - {texto.strip()} {tiny_url(link)}\n\n')
+            if not horario == None:
+                arquivo.write(f'{horario} - {texto.strip()} {tiny_url(link)}\n\n')
+                contador += 1
+
+    arquivo.write(f'\nEncontrado {contador} materias no site {site}')
+    arquivo.close()
+
+
+if __name__ == '__main__':
+    # tecmundo
+    valor = None
+    retorna_materias(link='https://www.tecmundo.com.br/novidades', tag_bloco='.tec--card--medium',
+                     tag_titulo='.tec--card__title__link', tag_horario='.z-flex-1', research=valor)
+
---
+++ new_bot.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+import telepot
+import os
+from classes import Funcoes
+import buscador
+
+mensagem = "Ola, use os comandos:\n/pdv_uptime : uptime dos pdvs\n/pdv_memoria_livre : memoria livre pdvs\n" \
+           "/pdv_limpa_memoria : Limpa Memoria PDV\n/exibe_hora - exibe a hora dos pdvs\n" \
+           "/acerta_hora - corrige a hora dos pdvs\n/reinicia_pdv : Reinicia o pdv com base no ultimo octeto (Ex 101,102)\n" \
+           "/verifica_cftv : Exibe status CFTV\n/verifica_buscapreco : lista busca precos\n/nivel_toner : nivel toner\n" \
+           "/triggers : triggers ativas\n/startvnc\n\n/chamados : palavra q informe o assunto - limpa - sem nada para exibir\n\n/noticias_tecmundo\n/noticias_tudocelular\n/noticias_olhar_digital\n" \
+           "/noticias_uol\n/noticias_techtudo\n/noticias_pesquisa"
+
+
+def handleCommad(content):
+    try:
+        chat_id = content['chat']['id']
+        try:
+            command, param = content['text'].split()
+        except:
+            command = content['text']
+            param = None
+
+        if command == 'Hi':
+            mensage = 'Hello, how are you?'
+            bot.sendMessage(chat_id, mensage)
+
+        if "pdv_uptime" in command:
+            bot.sendMessage(chat_id, f'UPTIME dos PDVs\nAguarde um momento, vou consultar para voce :)')
+            os.system('/bin/bash -c "sh comandos.sh uptime"')
+            inf = open("envioTelegram.txt")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "pdv_memoria_livre" in command:
+            bot.sendMessage(chat_id, 'Memoria Livre dos PDVs\nAguarde um momento, vou consultar para voce =)')
+            os.system('/bin/bash -c "sh comandos.sh memlivre"')
+            inf = open("envioTelegram.txt")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "pdv_limpa_memoria" in command:
+            bot.sendMessage(chat_id, 'Limpeza memoria Pdv\nAguarde um momento, vou consultar para voce =)')
+            os.system('/bin/bash -c "sh comandos.sh limpamem"')
+            inf = open("envioTelegram.txt")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "exibe_hora" in command:
+            bot.sendMessage(chat_id, 'Exibindo a hora dos Pdvs\nAguarde um momento, vou consultar para voce =)')
+            os.system('/bin/bash -c "sh comandos.sh exibehora"')
+            inf = open("envioTelegram.txt", encoding="ISO-8859-1")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "acerta_hora" in command:
+            bot.sendMessage(chat_id, 'Corrigindo a hora dos Pdvs\nAguarde um momento, isso pode demorar um pouco =)')
+            os.system('/bin/bash -c "sh comandos.sh acertahora"')
+            inf = open("envioTelegram.txt", encoding="ISO-8859-1")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "reinicia_pdv" in command:
+            if str(param):
+                os.system('/bin/bash -c "sh comandos.sh reiniciapdv "' + str(param))
+                inf = open("envioTelegram.txt")
+                bot.sendMessage(chat_id, " " + str(inf.read()))
+            else:
+                bot.sendMessage(chat_id, "Você nao informou qual o final do IP do PDV")
+
+        if "nivel_toner" in command:
+            bot.sendMessage(chat_id, 'Nivel de toner\nAguarde um momento, vou consultar para voce =)')
+            os.system('/bin/bash -c "sh comandos.sh toner"')
+            inf = open("envioTelegram.txt")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "verifica_cftv" in command:
+            bot.sendMessage(chat_id, 'Equipamentos de CFTV\nAguarde um momento, vou consultar para voce =)')
+            Funcoes.cftv()
+            inf = open("envioTelegram.txt")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "verifica_buscapreco" in command:
+            bot.sendMessage(chat_id, 'Busca Preco online')
+            Funcoes.buscapreco()
+            inf = open("envioTelegram.txt")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "triggers" in command:
+            bot.sendMessage(chat_id, 'Triggers Alarmadas')
+            Funcoes.verificaTriggers()
+            inf = open("envioTelegram.txt")
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "chamado" in command:
+            if str(param) == 'limpa':
+                Funcoes.limpachamado()
+                bot.sendMessage(chat_id, "Arquivo de chamados Limpo")
+            elif not str(param) == None:
+                Funcoes.chamado(str(param))
+                inf = open("chamados.txt")
+                print(inf)
+                bot.sendMessage(chat_id, " " + str(inf.read()))
+            else:
+                inf = open("chamados.txt")
+                bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "noticias_pesquisa" in command:
+
+            if param == None:
+                bot.sendMessage(chat_id, " " + 'É necessario passar um parametro de busca nos sites.')
+            else:
+                bot.sendMessage(chat_id, 'Buscando Noticias contendo: ' + str(param))
+                # tecmundo
+                buscador.retorna_materias(link='https://www.tecmundo.com.br/novidades', tag_bloco='.tec--card',
+                                          tag_titulo='.tec--card__title', tag_horario='.z-flex-1', research=param)
+                inf = open("resultado.txt", 'r')
+                bot.sendMessage(chat_id, " " + str(inf.read()))
+
+                # tudocelular
+                buscador.retorna_materias(link='https://www.tudocelular.com/', tag_bloco='.newlist_normal',
+                                          tag_titulo='.title_new', tag_horario='em', research=param)
+                inf = open("resultado.txt", 'r')
+                bot.sendMessage(chat_id, " " + str(inf.read()))
+
+                # olhardigital
+                buscador.retorna_materias(link='https://olhardigital.com.br/', tag_bloco='article',
+                                          tag_titulo='.title', tag_horario=None, research=param)
+                inf = open("resultado.txt", 'r')
+                bot.sendMessage(chat_id, " " + str(inf.read()))
+
+                # uol
+                buscador.retorna_materias(link='https://noticias.uol.com.br/', tag_bloco='.thumbnails-item',
+                                          tag_titulo='.thumb-title', tag_horario=None, research=param)
+                inf = open("resultado.txt", 'r')
+                bot.sendMessage(chat_id, " " + str(inf.read()))
+
+                # uol
+                buscador.retorna_materias(link='https://www.techtudo.com.br/', tag_bloco='.feed-post',
+                                          tag_titulo='.feed-post-body', tag_horario=None, research=param)
+                inf = open("resultado.txt", 'r')
+                bot.sendMessage(chat_id, " " + str(inf.read()))
+
+
+        if "noticias_tecmundo" in command:
+            bot.sendMessage(chat_id, 'Buscando Noticias contendo: ' + str(param))
+
+            # tecmundo
+            buscador.retorna_materias(link='https://www.tecmundo.com.br/novidades', tag_bloco='.tec--card',
+                                      tag_titulo='.tec--card__title', tag_horario='.z-flex-1', research=param)
+            inf = open("resultado.txt", 'r')
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "noticias_tudocelular" in command:
+            # tudocelular
+            buscador.retorna_materias(link='https://www.tudocelular.com/', tag_bloco='.newlist_normal',
+                                      tag_titulo='.title_new', tag_horario='em', research=param)
+            inf = open("resultado.txt", 'r')
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "noticias_olhar_digital" in command:
+            # olhardigital
+            buscador.retorna_materias(link='https://olhardigital.com.br/', tag_bloco='article',
+                                      tag_titulo='.title', tag_horario=None, research=param)
+            inf = open("resultado.txt", 'r')
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "noticias_uol" in command:
+            # uol
+            buscador.retorna_materias(link='https://noticias.uol.com.br/', tag_bloco='.thumbnails-item',
+                                      tag_titulo='.thumb-title', tag_horario=None, research=param)
+            inf = open("resultado.txt", 'r')
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        if "noticias_techtudo" in command:
+            # techtudo
+            buscador.retorna_materias(link='https://www.techtudo.com.br/', tag_bloco='.feed-post',
+                                      tag_titulo='.feed-post-body', tag_horario=None, research=param)
+            inf = open("resultado.txt", 'r')
+            bot.sendMessage(chat_id, " " + str(inf.read()))
+
+        bot.sendMessage(chat_id, mensagem)
+
+    except Exception as e:
+        bot.sendMessage(chat_id, f'There was an error!!! {e}')
+
+
+def sendMessage(chat, mensage):
+    bot.sendMessage(chat, mensage)
+
+
+# token_telegram = os.environ["TELEGRAM_TOKEN"]
+token_telegram = '1680792124:AAEd98zadjQ0HEpZhZ97-d-rQdPk02Jjtz8'
+bot = telepot.Bot(token_telegram)
+bot.message_loop(handleCommad)
+
+try:
+    print('press CTRL + C to cancel')
+
+    while True:
+        pass
+
+except KeyboardInterrupt:
+    print('\nBye')
